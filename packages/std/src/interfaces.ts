10cb62a37c009b638dd257496d5b819c531af5ba
code0xff <ian.jungyong.um@gmail.com>
2023-09-24 00:08:12 +0900

    feat: Add api interface

--- packages/std/src/interfaces.ts
+++ packages/std/src/interfaces.ts
@@ -5,3 +5,24 @@ export interface Storage {
 
   remove: (key: Uint8Array) => void;
 }
+
+export type Addr = string;
+export type CanonicalAddr = Uint8Array;
+
+export interface Api {
+  addrValidate: (human: string) => Addr;
+
+  addrCanonicalize: (human: string) => CanonicalAddr;
+
+  addrHumanize: (canonical: CanonicalAddr) => Addr;
+
+  secp256k1Verify: (messageHash: Uint8Array, signature: Uint8Array, publicKey: Uint8Array) => boolean;
+
+  secp256k1RecoverPubkey: (messageHash: Uint8Array, signature: Uint8Array, recoveryParam: u8) => Uint8Array;
+
+  ed25519Verify: (message: Uint8Array, signature: Uint8Array, publicKey: Uint8Array) => boolean;
+
+  ed25519BatchVerify: (messages: Array<Uint8Array>, signatures: Array<Uint8Array>, publicKeys: Array<Uint8Array>) => boolean;
+
+  debug: (message: string) => void;
+}
