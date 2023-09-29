6a1f01e3a3fe978dd636a9e57b257bab14534512
code0xff <ian.jungyong.um@gmail.com>
2023-09-30 00:29:08 +0900

    feat: Add export functions

--- assembly/std/exports.ts
+++ assembly/std/exports.ts
@@ -1,6 +1,9 @@
+import { JSON } from "json-as/assembly";
 import { Deps } from "./deps";
 import { ExternalApi, ExternalQuerier, ExternalStorage } from "./imports";
 import { Region } from "./memory";
+import { Reply, Response } from "./results";
+import { Env, MessageInfo } from "./types";
 
 export function allocate(size: usize): u32 {
   return Region.alloc(size) as u32;
@@ -8,6 +11,88 @@ export function allocate(size: usize): u32 {
 
 export function deallocate(pointer: u32): void {
   Region.consumeRegion(pointer);
+}
+
+export function doInstantiate<M>(instantiateFn: (deps: Deps, env: Env, info: MessageInfo, msg: M) => Response, envPtr: u32, infoPtr: u32, msgPtr: u32): u32 {
+  const envBytes = Region.consumeRegion(envPtr);
+  const infoBytes = Region.consumeRegion(infoPtr);
+  const msgBytes = Region.consumeRegion(msgPtr);
+
+  const env = JSON.parse<Env>(String.UTF8.decode(envBytes));
+  const info = JSON.parse<MessageInfo>(String.UTF8.decode(infoBytes));
+  const msg = JSON.parse<M>(String.UTF8.decode(msgBytes));
+
+  const deps = makeDependencies();
+  const res = instantiateFn(deps, env, info, msg);
+  const v = Uint8Array.wrap(String.UTF8.encode(JSON.stringify(res)));
+  return Region.buildRegion(v);
+}
+
+export function doExecute<M>(executeFn: (deps: Deps, env: Env, info: MessageInfo, msg: M) => Response, envPtr: u32, infoPtr: u32, msgPtr: u32): u32 {
+  const envBytes = Region.consumeRegion(envPtr);
+  const infoBytes = Region.consumeRegion(infoPtr);
+  const msgBytes = Region.consumeRegion(msgPtr);
+
+  const env = JSON.parse<Env>(String.UTF8.decode(envBytes));
+  const info = JSON.parse<MessageInfo>(String.UTF8.decode(infoBytes));
+  const msg = JSON.parse<M>(String.UTF8.decode(msgBytes));
+
+  const deps = makeDependencies();
+  const res = executeFn(deps, env, info, msg);
+  const v = Uint8Array.wrap(String.UTF8.encode(JSON.stringify(res)));
+  return Region.buildRegion(v);
+}
+
+export function doMigrate<M>(migrateFn: (deps: Deps, env: Env, msg: M) => Response, envPtr: u32, msgPtr: u32): u32 {
+  const envBytes = Region.consumeRegion(envPtr);
+  const msgBytes = Region.consumeRegion(msgPtr);
+
+  const env = JSON.parse<Env>(String.UTF8.decode(envBytes));
+  const msg = JSON.parse<M>(String.UTF8.decode(msgBytes));
+
+  const deps = makeDependencies();
+  const res = migrateFn(deps, env, msg);
+  const v = Uint8Array.wrap(String.UTF8.encode(JSON.stringify(res)));
+  return Region.buildRegion(v);
+}
+
+export function doSudo<M>(sudoFn: (deps: Deps, env: Env, msg: M) => Response, envPtr: u32, msgPtr: u32): u32 {
+  const envBytes = Region.consumeRegion(envPtr);
+  const msgBytes = Region.consumeRegion(msgPtr);
+
+  const env = JSON.parse<Env>(String.UTF8.decode(envBytes));
+  const msg = JSON.parse<M>(String.UTF8.decode(msgBytes));
+
+  const deps = makeDependencies();
+  const res = sudoFn(deps, env, msg);
+  const v = Uint8Array.wrap(String.UTF8.encode(JSON.stringify(res)));
+  return Region.buildRegion(v);
+}
+
+export function doReply(replyFn: (deps: Deps, env: Env, msg: Reply) => Response, envPtr: u32, msgPtr: u32): u32 {
+  const envBytes = Region.consumeRegion(envPtr);
+  const msgBytes = Region.consumeRegion(msgPtr);
+
+  const env = JSON.parse<Env>(String.UTF8.decode(envBytes));
+  const msg = JSON.parse<Reply>(String.UTF8.decode(msgBytes));
+
+  const deps = makeDependencies();
+  const res = replyFn(deps, env, msg);
+  const v = Uint8Array.wrap(String.UTF8.encode(JSON.stringify(res)));
+  return Region.buildRegion(v);
+}
+
+export function doQuery<M>(queryFn: (deps: Deps, env: Env, msg: M) => Response, envPtr: u32, msgPtr: u32): u32 {
+  const envBytes = Region.consumeRegion(envPtr);
+  const msgBytes = Region.consumeRegion(msgPtr);
+
+  const env = JSON.parse<Env>(String.UTF8.decode(envBytes));
+  const msg = JSON.parse<M>(String.UTF8.decode(msgBytes));
+
+  const deps = makeDependencies();
+  const res = queryFn(deps, env, msg);
+  const v = Uint8Array.wrap(String.UTF8.encode(JSON.stringify(res)));
+  return Region.buildRegion(v);
 }
 
 function makeDependencies(): Deps {
